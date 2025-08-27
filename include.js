64c8a4c618ba23cb829f1fbb9f2f1358368ebdcb
GiuDiMax <giuseppedimauro95@gmail.com>
2025-08-27 19:08:39 +0200

    edit

--- include.js
+++ include.js
@@ -1,10 +1,39 @@
-function includeHTML(id, file) {
+// include.js
+
+function includeHTML(id, file, callback) {
   fetch(file)
     .then(res => res.text())
-    .then(data => document.getElementById(id).innerHTML = data)
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
+      if (callback) {
+        callback();
+      }
+    })
     .catch(err => console.error(err));
 }
 
-// Includi header e footer
-includeHTML('header-container', 'header.html');
+// Questa funzione carica e avvia tutti gli script dipendenti dal DOM
+function initializeScripts() {
+  const scriptsToLoad = [
+    'assets/js/jquery.scrollex.min.js',
+    'assets/js/jquery.scrolly.min.js',
+    'assets/js/browser.min.js',
+    'assets/js/breakpoints.min.js',
+    'assets/js/util.js',
+    'assets/js/main.js'
+  ];
+
+  let promise = Promise.resolve();
+  scriptsToLoad.forEach(src => {
+    promise = promise.then(() => new Promise(resolve => {
+      const script = document.createElement('script');
+      script.src = src;
+      script.onload = resolve;
+      document.body.appendChild(script);
+    }));
+  });
+}
+
+// Carica l'header e, una volta terminato, avvia la logica della pagina
+includeHTML('header-container', 'header.html', initializeScripts);
 includeHTML('footer-container', 'footer.html');
