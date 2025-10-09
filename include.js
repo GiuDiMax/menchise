160408ebb8a818d0f25f1aaac8db9130dc4b15b6
GiuDiMax <giuseppedimauro95@gmail.com>
2025-10-09 15:08:35 +0200

    edit

--- include.js
+++ include.js
@@ -12,7 +12,31 @@ function includeHTML(id, file, callback) {
     .catch(err => console.error(err));
 }
 
-// Questa funzione carica e avvia tutti gli script dipendenti dal DOM
+function setupPageHead() {
+}
+
+function initializeAccordion() {
+  const accordionTriggers = document.querySelectorAll('.accordion-trigger');
+  console.log(`Trovati ${accordionTriggers.length} accordion-trigger.`);
+  accordionTriggers.forEach(trigger => {
+    trigger.addEventListener('click', (e) => {
+      e.preventDefault();
+      const parentLi = trigger.closest('.has-submenu');
+      const isActive = parentLi.classList.contains('active');
+      document.querySelectorAll('.has-submenu.active').forEach(openLi => {
+        if (openLi !== parentLi) {
+          openLi.classList.remove('active');
+        }
+      });
+      if (isActive) {
+        parentLi.classList.remove('active');
+      } else {
+        parentLi.classList.add('active');
+      }
+    });
+  });
+}
+
 function initializeScripts() {
   const scriptsToLoad = [
     'assets/js/jquery.scrollex.min.js',
@@ -32,28 +56,10 @@ function initializeScripts() {
       document.body.appendChild(script);
     }));
   });
-
-  const accordionTriggers = document.querySelectorAll('.accordion-trigger');
-  accordionTriggers.forEach(trigger => {
-    trigger.addEventListener('click', (e) => {
-      e.preventDefault();
-      const parentLi = trigger.closest('.has-submenu');
-      const isActive = parentLi.classList.contains('active');
-      document.querySelectorAll('.has-submenu.active').forEach(openLi => {
-        if (openLi !== parentLi) {
-          openLi.classList.remove('active');
-        }
-      });
-      if (isActive) {
-        parentLi.classList.remove('active');
-      } else {
-        parentLi.classList.add('active');
-      }
-    });
-  });
-
+  setupPageHead()
+  initializeAccordion()
 }
 
-// Carica l'header e, una volta terminato, avvia la logica della pagina
 includeHTML('header-container', 'header.html', initializeScripts);
 includeHTML('footer-container', 'footer.html');
+
