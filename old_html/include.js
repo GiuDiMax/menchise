ab0be044775a71329af543583782718e3682fb66
GiuDiMax <giuseppedimauro95@gmail.com>
2026-06-16 16:29:12 +0200

    passaggio ad astro

---
+++ old_html/include.js
@@ -0,0 +1,64 @@
+// include.js
+
+function includeHTML(id, file, callback) {
+  fetch(file)
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
+      if (callback) {
+        callback();
+      }
+    })
+    .catch(err => console.error(err));
+}
+
+function setupPageHead() {
+}
+
+function initializeAccordion() {
+  const accordionTriggers = document.querySelectorAll('.accordion-trigger');
+  console.log(`Trovati ${accordionTriggers.length} accordion-trigger.`);
+  accordionTriggers.forEach(trigger => {
+    trigger.addEventListener('click', (e) => {
+      e.preventDefault();
+      const parentLi = trigger.closest('.has-submenu');
+      const isActive = parentLi.classList.contains('active');
+      document.querySelectorAll('.has-submenu.active').forEach(openLi => {
+        if (openLi !== parentLi) {
+          openLi.classList.remove('active');
+        }
+      });
+      if (isActive) {
+        parentLi.classList.remove('active');
+      } else {
+        parentLi.classList.add('active');
+      }
+    });
+  });
+}
+
+function initializeScripts() {
+  const scriptsToLoad = [
+    'assets/js/jquery.scrollex.min.js',
+    'assets/js/jquery.scrolly.min.js',
+    'assets/js/browser.min.js',
+    'assets/js/breakpoints.min.js',
+    'assets/js/util.js',
+    'assets/js/main.js'
+  ];
+  initializeAccordion()
+  let promise = Promise.resolve();
+  scriptsToLoad.forEach(src => {
+    promise = promise.then(() => new Promise(resolve => {
+      const script = document.createElement('script');
+      script.src = src;
+      script.onload = resolve;
+      document.body.appendChild(script);
+    }));
+  });
+  //setupPageHead()
+}
+
+includeHTML('header-container', 'header.html', initializeScripts);
+includeHTML('footer-container', 'footer.html');
+
